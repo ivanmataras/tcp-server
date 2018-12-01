7dc4a1491caf72b3df075198af303e630d23dee5
Ivan <imataras@yandex.ru>
2018-12-01 21:59:13 +0300

    Implement organization dao

--- dao/src/main/java/com/ediweb/education/dao/OrganizationDAO.java
+++ dao/src/main/java/com/ediweb/education/dao/OrganizationDAO.java
@@ -2,9 +2,8 @@ package com.ediweb.education.dao;
 
 import com.ediweb.education.entities.Organization;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,9 +13,12 @@ public class OrganizationDAO implements DAO<Organization> {
     private static final Logger log = Logger.getLogger(OrganizationDAO.class.getName());
 
     private static final String SQL_SELECT_ALL_ORGANIZATIONS = "SELECT id, name, full_name FROM organization";
+    private static final String SQL_DELETE_ALL_ORGANIZATIONS = "DELETE FROM organization";
     private static final String SQL_SELECT_ORGANIZATION_BY_ID = "SELECT id, name, full_name FROM organization WHERE id = ?";
+    private static final String SQL_SELECT_ORGANIZATION = "SELECT id, name, full_name FROM organization WHERE id = ?";
     private static final String SQL_INSERT_ORGANIZATION = "INSERT INTO organization (name, full_name) VALUES(?, ?)";
     private static final String SQL_UPDATE_ORGANIZATION = "UPDATE organization SET name = ?, full_name = ? WHERE id = ?";
+    private static final String SQL_DELETE_ORGANIZATION_BY_ID = "DELETE FROM organization WHERE id = ?";
     private static final String SQL_DELETE_ORGANIZATION = "DELETE FROM organization WHERE id = ?";
 
     private Connection connection;
@@ -31,43 +33,139 @@ public class OrganizationDAO implements DAO<Organization> {
 
     @Override
     public List<Organization> findAll() {
-        return null;
+        List<Organization> organizations = new ArrayList<Organization>();
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_ALL_ORGANIZATIONS);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Organization organization = new Organization();
+                organization.setId(resultSet.getInt("id"));
+                organization.setName(resultSet.getString("name"));
+                organization.setFullName(resultSet.getString("full_name"));
+                organizations.add(organization);
+            }
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
+        return organizations;
     }
 
     @Override
     public void deleteAll() {
-
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_DELETE_ALL_ORGANIZATIONS);
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     @Override
     public Organization find(int id) {
-        return null;
+        Organization organization = null;
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_ORGANIZATION_BY_ID);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                organization = new Organization();
+                organization.setId(resultSet.getInt("id"));
+                organization.setName(resultSet.getString("name"));
+                organization.setFullName(resultSet.getString("full_name"));
+            }
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
+        return organization;
     }
 
     @Override
     public Organization find(Organization organization) {
-        return null;
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_SELECT_ORGANIZATION);
+            statement.setInt(1, organization.getId());
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                organization = new Organization();
+                organization.setId(resultSet.getInt("id"));
+                organization.setName(resultSet.getString("name"));
+                organization.setFullName(resultSet.getString("full_name"));
+            }
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
+        return organization;
     }
-
 
     @Override
     public void create(Organization organization) {
-
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_INSERT_ORGANIZATION);
+            statement.setString(1, organization.getName());
+            statement.setString(2, organization.getFullName());
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     @Override
     public void update(Organization organization) {
-
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_UPDATE_ORGANIZATION);
+            statement.setString(1, organization.getName());
+            statement.setString(2, organization.getFullName());
+            statement.setInt(3, organization.getId());
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     @Override
     public void delete(int id) {
-
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_DELETE_ORGANIZATION_BY_ID);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     @Override
     public void delete(Organization organization) {
-
+        PreparedStatement statement = null;
+        try {
+            statement = connection.prepareStatement(SQL_DELETE_ORGANIZATION);
+            statement.setInt(1, organization.getId());
+            statement.executeUpdate();
+        } catch (SQLException sqlException) {
+            if (log.isLoggable(Level.SEVERE)) log.severe(sqlException.getMessage());
+        } finally {
+            close(statement);
+        }
     }
 
     @Override
