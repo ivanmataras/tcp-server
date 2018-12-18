a4e6623a02a79deecd460bf9d98889646f4f7184
ivanmataras <imataras@yandex.ru>
2018-12-19 02:41:41 +0300

    Add protocol entities

--- protocol/src/main/java/com/ediweb/education/protocol/DocumentStatus.java
+++ protocol/src/main/java/com/ediweb/education/protocol/DocumentStatus.java
@@ -1,7 +1,10 @@
 package com.ediweb.education.protocol;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
+@XmlRootElement(name = "DocumentStatus")
 public class DocumentStatus implements Entity, Serializable, Cloneable {
 
     private int id;
@@ -13,6 +16,7 @@ public class DocumentStatus implements Entity, Serializable, Cloneable {
     }
 
     @Override
+    @XmlElement(name = "Id")
     public int getId() {
         return id;
     }
@@ -22,6 +26,7 @@ public class DocumentStatus implements Entity, Serializable, Cloneable {
         this.id = id;
     }
 
+    @XmlElement(name = "Status")
     public String getStatus() {
         return status;
     }
