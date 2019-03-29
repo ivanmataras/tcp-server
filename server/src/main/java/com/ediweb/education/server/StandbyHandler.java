1e8a472a5ed8fe61143b50987ac7de5def44b135
Ivan Mataras <ivan.mataras@ediweb.com>
2019-03-29 20:05:39 +0300

    Add logic for standbyhandler

--- server/src/main/java/com/ediweb/education/server/StandbyHandler.java
+++ server/src/main/java/com/ediweb/education/server/StandbyHandler.java
@@ -1,14 +1,23 @@
 package com.ediweb.education.server;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static java.lang.System.out;
 
 public class StandbyHandler implements Runnable {
 
     private static final Logger log = Logger.getLogger(StandbyHandler.class.getName());
 
     private final CountDownLatch standbyHandlerLatch;
+    private final StringBuilder clock = new StringBuilder(32);
+    private final char CR = '\r';
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss z", Locale.ENGLISH);
 
     public StandbyHandler(CountDownLatch standbyHandlerLatch) {
         this.standbyHandlerLatch = standbyHandlerLatch;
@@ -16,6 +25,19 @@ public class StandbyHandler implements Runnable {
 
     @Override
     public void run() {
+        try {
+            while (true) {
+                clock.append(CR).append(ZonedDateTime.now().format(formatter));
+                out.print(clock);
+                clock.delete(0, clock.length());
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (InterruptedException interruptedException) {
+            if (log.isLoggable(Level.SEVERE)) {
+                log.severe("Interrupted exception.");
+            }
+        }
+
         if (log.isLoggable(Level.INFO)) {
             log.info("StandbyHandler task has finished.");
         }
--- server/src/main/java/com/ediweb/education/server/StopMonitor.java
+++ server/src/main/java/com/ediweb/education/server/StopMonitor.java
@@ -1,6 +1,9 @@
 package com.ediweb.education.server;
 
+import java.util.logging.Logger;
+
 public class StopMonitor {
 
+    private static final Logger log = Logger.getLogger(StopMonitor.class.getName());
 
 }
