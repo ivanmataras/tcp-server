b1e526ebb11b59bcb7666b600e931e9260ea0bd6
Ivan <imataras@yandex.ru>
2019-04-05 00:45:59 +0300

    Delete swing gui

--- client/src/main/java/com/ediweb/education/client/gui/ui/DocType.java
+++
@@ -1,9 +0,0 @@
-package com.ediweb.education.client.gui.ui;
-
-public enum DocType {
-
-	ALL,
-	ORDER,
-	ORDRSP;
-
-}
--- client/src/main/java/com/ediweb/education/client/gui/ui/models/Form.java
+++
@@ -1,7 +0,0 @@
-package com.ediweb.education.client.gui.ui.models;
-
-public interface Form {
-
-	public String getName();
-
-}
